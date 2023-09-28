0ed338faa8c19fb7569f3d792fee2c00a1869fc6
Sergejs Popovs <s.popovs@dyninno.lv>
2023-09-28 15:48:35 +0300

    Add data to temlates

---
+++ sharing-data-template-37/cmd/web/main.go
@@ -0,0 +1,38 @@
+package main
+
+// Run server
+// go run cmd/web/main.go
+// go run ./cmd/web
+
+import (
+	"fmt"
+	"github/tsawler/go-course/pkg/config"
+	"github/tsawler/go-course/pkg/handlers"
+	"github/tsawler/go-course/pkg/render"
+	"log"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache() // страно, получается каждый запрос будет генерировать этот кеш ? посмотрим что будет дальше
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println(fmt.Sprintf("Starting aplication on port : %s", portNumber))
+
+	_ = http.ListenAndServe(portNumber, nil)
+}
