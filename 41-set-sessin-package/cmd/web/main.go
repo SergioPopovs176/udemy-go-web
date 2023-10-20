b88063b2f0aaff8aebf17c91338fc45bec8023e5
Sergejs Popovs <s.popovs@dyninno.lv>
2023-10-20 15:34:26 +0300

    Add session manager to server

--- 41-set-sessin-package/cmd/web/main.go
+++ 41-set-sessin-package/cmd/web/main.go
@@ -8,17 +8,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/SergioPopovs176/udemy-go-web/pkg/config"
 	"github.com/SergioPopovs176/udemy-go-web/pkg/handlers"
 	"github.com/SergioPopovs176/udemy-go-web/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- 41-set-sessin-package/go.mod
+++ 41-set-sessin-package/go.mod
@@ -3,6 +3,7 @@ module github.com/SergioPopovs176/udemy-go-web
 go 1.19
 
 require (
+	github.com/alexedwards/scs/v2 v2.5.1 // indirect
 	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/justinas/nosurf v1.1.1 // indirect
 )
